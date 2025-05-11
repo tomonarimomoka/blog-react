58d5a6163125009f1fdbdd4553a68c6dc56a85e7
*TCevNZpVHc <tomomiku0112@gmail.com>
2025-05-11 14:05:57 +0900

    自動置換に失敗したり、意図せぬ置換をしたので手で修正

--- tech-blog/src/artical/awsListOfServices.tsx
+++ tech-blog/src/artical/awsListOfServices.tsx
@@ -45,32 +45,30 @@ export const AwsListOfServices: FC = () => {
     </p>
 
     <SubTitle subTitle="AWS主要サービス一覧" />
-    <p>
-      AWSでは200を超えるサービスがあります。以下に主要なサービスを抜粋して一覧化します。
-      <table >
-        <tbody>
-          <tr><th>サービス名</th><th>サービス内容</th><th>ちょい解説</th></tr>
-          <tr><td>IAM</td><td>AWS上のリソースにアクセスする為の権限を管理するためのサービス</td><td></td></tr>
-          <tr><td>EC2</td><td>クラウド上にコンピューターを借りられるサービス。</td><td>借りるコンピューターのことを「インスタンス」と呼ぶ</td></tr>
-          <tr><td>AMI</td><td>OSやミドルウェアのテンプレートを作成できるサービス</td><td>AMIの設定をもとにインスタンスを作成することができる</td></tr>
-          <tr><td>ROute53</td><td>多様なルーティングができるサービス</td><td></td></tr>
-          <tr><td>AWS WAF</td><td>アプリケーションに特化したファイヤーウォール</td><td></td></tr>
-          <tr><td>AWS Shield</td><td>DDoS攻撃に対応するためのサービス</td><td></td></tr>
-          <tr><td>Amazom API Gateway</td><td>REST、WebSocketのAPIを作ったり、公開したり、管理できるサービス。</td><td>HTTP通信を通じてLambda関数を呼び出せる。</td></tr>
-          <tr><td>Amazom Lambda</td><td>特定のタイミングで実行できる関数を作れるサービス。</td><td>サーバーは不要だが、小さなデータサイズの関数のみしか作れない。</td></tr>
-          <tr><td>Amazom CloudFront</td><td>低レンテンシーでコンテンツを配信できるサービス。</td><td>キャッシュを利用することで低レンテンシーを実現している。</td></tr>
-          <tr><td>Amazom Dynamo DB</td><td>NoSQLのDBが使えるサービス</td><td>データはJSON形式で保持される</td></tr>
-          <tr><td>Amazom S3</td><td>データを保存できるストレージサービス。</td><td>ストレージ容量は無制限</td></tr>
-          <tr><td>Amazom CloudWach</td><td>EC2インスタンス、RDSインスタンス、DynamoDBテーブルなどの現状をモニタリングサービス</td><td>logの収集だけでなく、アラームを設定することもできる（CPUが連続して高い使用率の時にアラームを鳴らすなど）</td></tr>
-          <tr><td>AWS Amplify</td><td>Webアプリケーション、モバイルアプリケーションを構築、デプロイできるフレームワーク</td><td></td></tr>
-          <tr><td>Amazon Cognito</td><td>認証機能を提供するサービス</td><td>Oauth 2.0、OpenID Connectにも対応している</td></tr>
-          <tr><td>AWS SDK</td><td>開発者が開発の際にスムーズに開発できるようにまとまったツールで、例えばAPIライブラリなどを含む</td><td>Software Development Kit の略</td></tr>
-          <tr><td>Amazon Kinesis</td><td>大量のデータの転送処理や操作を高速に行うためのサービス</td><td></td></tr>
-          <tr><td>Elastic IP</td><td>AWSで使うパブリックIPアドレス</td><td>AWSから割り当てられるIPアドレスはサーバーを起動するたびに変更されるが、ElasticIPは固定できる</td></tr>
-          <tr><td>Amazon Connect</td><td>ソフトフォン（PC上で動く電話）を提供するサービス</td><td></td></tr>
-        
-        </tbody>
-            </table>
+    <p>AWSでは200を超えるサービスがあります。以下に主要なサービスを抜粋して一覧化します。</p>
+    <table >
+      <tbody>
+        <tr><th>サービス名</th><th>サービス内容</th><th>ちょい解説</th></tr>
+        <tr><td>IAM</td><td>AWS上のリソースにアクセスする為の権限を管理するためのサービス</td><td></td></tr>
+        <tr><td>EC2</td><td>クラウド上にコンピューターを借りられるサービス。</td><td>借りるコンピューターのことを「インスタンス」と呼ぶ</td></tr>
+        <tr><td>AMI</td><td>OSやミドルウェアのテンプレートを作成できるサービス</td><td>AMIの設定をもとにインスタンスを作成することができる</td></tr>
+        <tr><td>ROute53</td><td>多様なルーティングができるサービス</td><td></td></tr>
+        <tr><td>AWS WAF</td><td>アプリケーションに特化したファイヤーウォール</td><td></td></tr>
+        <tr><td>AWS Shield</td><td>DDoS攻撃に対応するためのサービス</td><td></td></tr>
+        <tr><td>Amazom API Gateway</td><td>REST、WebSocketのAPIを作ったり、公開したり、管理できるサービス。</td><td>HTTP通信を通じてLambda関数を呼び出せる。</td></tr>
+        <tr><td>Amazom Lambda</td><td>特定のタイミングで実行できる関数を作れるサービス。</td><td>サーバーは不要だが、小さなデータサイズの関数のみしか作れない。</td></tr>
+        <tr><td>Amazom CloudFront</td><td>低レンテンシーでコンテンツを配信できるサービス。</td><td>キャッシュを利用することで低レンテンシーを実現している。</td></tr>
+        <tr><td>Amazom Dynamo DB</td><td>NoSQLのDBが使えるサービス</td><td>データはJSON形式で保持される</td></tr>
+        <tr><td>Amazom S3</td><td>データを保存できるストレージサービス。</td><td>ストレージ容量は無制限</td></tr>
+        <tr><td>Amazom CloudWach</td><td>EC2インスタンス、RDSインスタンス、DynamoDBテーブルなどの現状をモニタリングサービス</td><td>logの収集だけでなく、アラームを設定することもできる（CPUが連続して高い使用率の時にアラームを鳴らすなど）</td></tr>
+        <tr><td>AWS Amplify</td><td>Webアプリケーション、モバイルアプリケーションを構築、デプロイできるフレームワーク</td><td></td></tr>
+        <tr><td>Amazon Cognito</td><td>認証機能を提供するサービス</td><td>Oauth 2.0、OpenID Connectにも対応している</td></tr>
+        <tr><td>AWS SDK</td><td>開発者が開発の際にスムーズに開発できるようにまとまったツールで、例えばAPIライブラリなどを含む</td><td>Software Development Kit の略</td></tr>
+        <tr><td>Amazon Kinesis</td><td>大量のデータの転送処理や操作を高速に行うためのサービス</td><td></td></tr>
+        <tr><td>Elastic IP</td><td>AWSで使うパブリックIPアドレス</td><td>AWSから割り当てられるIPアドレスはサーバーを起動するたびに変更されるが、ElasticIPは固定できる</td></tr>
+        <tr><td>Amazon Connect</td><td>ソフトフォン（PC上で動く電話）を提供するサービス</td><td></td></tr>
+      </tbody>
+    </table>
     <h4>参考</h4>
     山下 光洋 and 海老原 寛之 , AWS認定資格試験テキスト　AWS認定 クラウドプラクティショナー, 2021
     <br/>
